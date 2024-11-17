6f85bd4c55e2c220c3e6979197f7ac8995c975a8
kamdev-choudhary <kamdevchoudhary@gmail.com>
2024-11-18 00:28:59 +0530

    M

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Make sure the base is set to your GitHub repo name
   plugins: [react()],
 })
