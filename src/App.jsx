1c347c1cd8c5bff325aa27bcb3bb56e50e6398cf
kamdev-choudhary <kamdevchoudhary@gmail.com>
2024-11-18 13:35:01 +0530

    p

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const DefaultLayout = React.lazy(() => import("./layout/DefaultLayout"));
 
-const Home = React.lazy(() => import("./pages/Home"));
-
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Router basename="/portfolio">
         <Routes>
-          {/* Define routes here */}
           <Route path="/*" element={<DefaultLayout />} />
-          {/* Add more routes as needed */}
         </Routes>
       </Router>
     </Suspense>
